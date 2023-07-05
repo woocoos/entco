48ddd37fd2c6475511380b28ad620a7d1972e006
tsingsun <21997272@qq.com>
2023-07-05 18:09:04 +0800

    fix decimal back to float type that can support filter ,validate,muatation add in numbric

--- genx/decimal.go
+++ genx/decimal.go
@@ -4,6 +4,7 @@ import (
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
 	"entgo.io/ent/schema/field"
+	"text/template"
 )
 
 type DecimalExtension struct {
@@ -16,15 +17,33 @@ func (DecimalExtension) Hooks() []gen.Hook {
 	}
 }
 
-func (DecimalExtension) Templates() []*gen.Template {
+func (DecimalExtension) Func() template.FuncMap {
+	return template.FuncMap{
+		"isCustomerField": func(f *gen.Field) bool {
+			if !f.HasGoType() {
+				return false
+			}
+			if f.Type.Numeric() && f.Type.RType != nil && f.Type.RType.PkgPath == "github.com/shopspring/decimal" {
+				return true
+			}
+			return false
+		},
+	}
+}
+
+func (d DecimalExtension) Templates() []*gen.Template {
 	return []*gen.Template{
 		gen.MustParse(gen.NewTemplate("runtime").
+			Funcs(d.Func()).
 			ParseFS(_templates, "template/runtime.tmpl")),
 		gen.MustParse(gen.NewTemplate("meta").
+			Funcs(d.Func()).
 			ParseFS(_templates, "template/meta.tmpl")),
 		gen.MustParse(gen.NewTemplate("create").
+			Funcs(d.Func()).
 			ParseFS(_templates, "template/create.tmpl")),
 		gen.MustParse(gen.NewTemplate("update").
+			Funcs(d.Func()).
 			ParseFS(_templates, "template/update.tmpl")),
 	}
 }
--- integration/gentest/ent/runtime/runtime.go
+++ integration/gentest/ent/runtime/runtime.go
@@ -5,5 +5,5 @@ package runtime
 // The schema-stitching logic is generated in github.com/woocoos/entco/integration/gentest/ent/runtime.go
 
 const (
-	Version = "v0.12.3" // Version of ent codegen.
+	Version = "v0.12.4-0.20230702151415-1ec75238037c" // Version of ent codegen.
 )
