5de4f9d7e73ba0d334de52ca88d84ee43920fdac
tsingsun <21997272@qq.com>
2023-09-23 00:56:35 +0800

    default and simple signer config

--- gqlx/middleware.go
+++ gqlx/middleware.go
@@ -50,6 +50,15 @@ func RegisterTokenSignerMiddleware() web.Option {
 	return web.WithMiddlewareNewFunc(signer.TokenSignerName, func() handler.Middleware {
 		mw := signer.NewMiddleware(signer.TokenSignerName, handler.WithMiddlewareConfig(func(config any) {
 			c := config.(*signer.Config)
+			c.SignerConfig.UnsignedPayload = true
+			c.SignerConfig.AuthScheme = "KO-HMAC-SHA1"
+			c.SignerConfig.AuthHeaders = []string{"timestamp", "nonce"}
+			c.SignerConfig.SignedLookups = map[string]string{
+				"accessToken": "header:Authorization>Bearer",
+				"timestamp":   "",
+				"nonce":       "",
+				"url":         "CanonicalUri",
+			}
 			c.Skipper = func(c *gin.Context) bool {
 				if c.IsWebsocket() {
 					return true
